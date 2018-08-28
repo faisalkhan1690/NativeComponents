e7a7d6613a32c86ae24d4a96bc37f81a0cd7f114
Faisal Khan <faisal.khan@kelltontech.com>
2018-08-28 20:46:54 +0530

    basic screen added

--- App.js
+++ App.js
@@ -1,49 +1,41 @@
 /**
  * Sample React Native App
- * https://github.com/facebook/react-native
+ * httmps://github.com/facebook/react-native
  *
  * @format
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component}from 'react';
+import { createStackNavigator } from 'react-navigation';
+import Home from './Home';
+import SectionListComponent from './SectionListComponent';
+import DrawerLayoutComponent from './DrawerLayoutComponent.';
+import DatePickerComponent from './DatePickerComponent';
+import NavigatorComponent from './NavigatorComponent';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <RootStack/>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const RootStack = createStackNavigator({
+  Home: {
+    screen: Home
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  SectionListComponent: {
+    screen: SectionListComponent
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  DrawerLayoutComponent: {
+    screen: DrawerLayoutComponent
   },
+  DatePickerComponent: {
+    screen: DatePickerComponent
+  },
+  NavigatorComponent: {
+    screen: NavigatorComponent
+  }
 });
---
+++ DatePickerComponent.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {StyleSheet,View,Text} from 'react-native';
+
+
+
+export default class DatePickerComponent extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: 'DatePickerIOS',
+        headerTitleStyle: {
+            textAlign: 'left',
+            alignSelf: 'center',
+            fontSize: 20,
+            color: 'white',
+            flex:1
+        },
+        headerTintColor:'white',
+        headerStyle: {
+            height: 55,
+            backgroundColor: '#5D7ED3',
+        },
+        headerRight: (<View/>)
+    });
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.textStyle}> Hello</Text>
+            </View>
+        );
+    }
+
+    
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor:'#fff'
+    },
+    textStyle:{
+      padding:10,
+      fontSize:16,
+      color:'#000',
+      borderWidth:0.2,
+      borderColor:'#ccc'
+    }
+  });
+  
